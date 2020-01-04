ed42b36ed762f53e5473c18e139ff40c8af6865a
Morgane Gedio <morgane.gedio@gmail.com>
2020-01-04 11:39:28 +0100

    add permitted categories for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  PERMITTED_CATEGORIES = ["food", "olive oil", "bread", "music", "outing", "art", "kids"]
+
+  validates :category, inclusion: { in: PERMITTED_CATEGORIES }
 end
